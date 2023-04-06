e2dc043d4d4610c44852f71c9e6ed552ce969177
jasushibi <106804805+jasushibi@users.noreply.github.com>
2023-04-06 08:14:43 +0530

    Update intercept.js

--- intercept.js
+++ intercept.js
@@ -1,3 +1,12 @@
-function hello(){
-    console.log('Hi')
+function intercept() {
+    const { fetch: originalFetch } = window;
+
+    window.fetch = async (...args) => {
+        let [resource, config ] = args;
+        // request interceptor here
+        const response = await originalFetch(resource, config);
+        // response interceptor here
+        console.log(response.json());
+        return response;
+    };
 }
